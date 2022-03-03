0c0d95893d7a4f74d8e808752ca023d33094c2de
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-03-03 11:17:24 +0300

    added bootstrap styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,34 @@ import React from "react";
 function Header(){
     return(
         <React.Fragment>
-             <div>Our Clothing, Your Comfort</div>
+             <div className='section-1'>
+               <div className="container">
+                   <div className="row">
+                       <div className="col-12">
+                           <div className="title d-flex">
+                               <div className="justify-content-center align-self-center w-100">
+                                   <div className="big-title text-center">Our Clothing, Your Comfort</div>
+                                   <div className="title-desc text-center">
+                                       We understand that you need to look good and feel great. <br/>
+                                       It all starts with what you are wearing.                                
+                                   </div>
+                               </div> 
+                           </div>
+                       </div>
 
-            <div >
+                       <div className="col-12">
+                           <div className="text-center chevron-down">
+                               <a href="#products">
+                                   <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                               </a>
+                           </div>
 
-                We understand that you need to look good and feel great. <br/>
+                       </div>
 
-                It all starts with what you are wearing.                                
+                   </div>
+               </div>
 
-            </div>
+           </div>
 
         </React.Fragment>
     )
