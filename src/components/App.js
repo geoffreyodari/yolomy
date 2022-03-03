8024029311e6453eebe2ca1743a983d84938d250
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-03-03 11:01:31 +0300

    moved app to components folder

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import '../App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Yolomy Products</h1>
+      <p>Trousers</p>
+      <p>shirts</p>
+    </React.Fragment>
+  );
+}
+
+export default App;
