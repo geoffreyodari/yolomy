0cb20ec30bd2312f18329c4f561ee39a5636bcfb
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-03-03 12:59:54 +0300

    added about us section

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 
 import React from 'react';
 import Header from './Header';
+import AboutUs from './AboutUs';
 import '../styles/App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <div>
+      <div className='container-fluid'>
         <Header />
+        <AboutUs />
       </div>
     </React.Fragment>
   );
